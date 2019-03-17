43086cd1c0b27d8187cae80ecc550ead349cecf6
yoy333 <42850829+yoy333@users.noreply.github.com>
2019-03-17 09:29:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// local functions
 var verticleMomentum = 0;
 var horizontalMomentum = 0;
 var stageTouch = true
@@ -10,11 +11,18 @@ function gravity(){
 		verticleMomentum += 1;
 	}
 }
+function cap(){
+	if(horizontalMomentum>3){
+  	horizontalMomentum=3
+  }else if(horizontalMomentum<-3){
+  	horizontalMomentum = -3
+  }
+}
 function moveP_one(){
-	if(touching('div#P1','div#stage')||touching('div#P1', 'div#stage')){
+	if(touching('div#P1','div#barrier')||touching('div#P1', 'div#stage')){
 		var P_one_Att =  getpos('div#P1')
 		var P_one_right = P_one_Att.right
-		if(stageTouch==false&&P_one_right<250==''){
+		if(stageTouch==false&&P_one_Att.right<250){
 			var P_one_Att = getpos('div#P1')
 			console.log(250-P_one_Att.width+'px')
 			$('div#P1').css('left', 250-P_one_Att.width+'px')
@@ -33,10 +41,12 @@ function moveP_one(){
 		stageTouch = false;
 	}
 	if(keys.a){
-		horizontalMomentum -= 17;
+  	cap();
+		horizontalMomentum -= 3;
 	}
 	if(keys.d){
-		horizontalMomentum += 17;
+  	cap();
+		horizontalMomentum += 3;
 	}
 	$('div#P1').css('top', verticleMomentum+P_one_Att.top+'px')
 	$('div#P1').css('left', horizontalMomentum+P_one_Att.left+'px')
