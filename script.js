13672c492708557ab5de781e3be10bdfd4348b96
yoy333 <42850829+yoy333@users.noreply.github.com>
2019-03-19 13:34:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,32 @@
-// local functions
 var verticleMomentum = 0;
 var horizontalMomentum = 0;
 var stageTouch = true
 function gravity(){
-	var P_one_Att =  getpos('div#P1')
-	if(P_one_Att.right<250){
-		stageTouch=false
-	}
 	if(stageTouch==false){
 		verticleMomentum += 1;
 	}
 }
 function cap(){
-	if(horizontalMomentum>3){
-  	horizontalMomentum=3
-  }else if(horizontalMomentum<-3){
-  	horizontalMomentum = -3
+	if(horizontalMomentum>7){w
+  	horizontalMomentum=7
+  }else if(horizontalMomentum<-7){
+  	horizontalMomentum = -7
   }
 }
 function moveP_one(){
+	var P_one_Att = getpos('#P1')
 	if(touching('div#P1','div#barrier')||touching('div#P1', 'div#stage')){
-		var P_one_Att =  getpos('div#P1')
-		var P_one_right = P_one_Att.right
-		if(stageTouch==false&&P_one_Att.right<250){
-			var P_one_Att = getpos('div#P1')
-			console.log(250-P_one_Att.width+'px')
-			$('div#P1').css('left', 250-P_one_Att.width+'px')
-		}else{
-			verticleMomentum = 0;
-			var P_one_Att = getpos('div#P1')
-			$('div#P1').css('top', 440-P_one_Att.height+'px')
-			stageTouch=true
-		}
+  	document.getElementById('counter').innerHTML = P_one_Att.bottom+verticleMomentum>440;
+  	if(P_one_Att.right>250&&P_one_Att.left<750&&P_one_Att.bottom+verticleMomentum>440){
+    	$('div#P1').css("top", 440-P_one_Att.height+'px')
+      if(verticleMomentum>0){
+      	verticleMomentum=0
+      }
+      stageTouch=true
+  	}
 	}else{
 		stageTouch=false
 	}
-	var P_one_Att =  getpos('div#P1')
-	if(keys.w&&stageTouch==true){
+  if(keys.w&&stageTouch){
 		verticleMomentum -= 17;
-		stageTouch = false;
 	}
-	if(keys.a){
-  	cap();
-		horizontalMomentum -= 3;
-	}
-	if(keys.d){
-  	cap();
-		horizontalMomentum += 3;
-	}
-	$('div#P1').css('top', verticleMomentum+P_one_Att.top+'px')
-	$('div#P1').css('left', horizontalMomentum+P_one_Att.left+'px')
-}
-$(document).ready(function(){
-	draw();
-})
-var draw = function(){
-	gravity();
-	moveP_one();
-	requestAnimationFrame(draw);
-}
